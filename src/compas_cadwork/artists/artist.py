66369e8e2fb90a38a8f2010c46ceb7173b87de4d
Chen Kasirer <ckasirer@ethz.ch>
2023-05-30 18:37:27 +0200

    added docstrings

--- src/compas_cadwork/artists/artist.py
+++ src/compas_cadwork/artists/artist.py
@@ -2,12 +2,22 @@ from compas.artists import Artist
 
 
 class CadworkArtist(Artist):
+    """Base class for all of cadwork's Artists."""
 
     DRAWN_ELEMENTS = []
 
     def add_element(self, element_id):
+        """Records the given element_id to track elements added by the :class:`~compas_cadwork.artists.CadworkArtist`.
+
+        Parameters
+        ----------
+        element_id : int
+            The element ID to add to tracking.
+
+        """
         self.DRAWN_ELEMENTS.append(element_id)
 
     def clear(self):
+        """Removes all elements tracked by the :class:`~compas_cadwork.artists.CadworkArtist` from the cadwork model."""
         # TODO: remove all elements from scene
         self.DRAWN_ELEMENTS = []
--- src/compas_cadwork/artists/instructionartist.py
+++ src/compas_cadwork/artists/instructionartist.py
@@ -7,13 +7,30 @@ from compas_cadwork.conversions import vector_to_cadwork
 from element_controller import create_text_object
 
 
-
 class Text3dInstrcutionArtist(CadworkArtist):
+    """Draws a 3d text instruction onto the view.
+
+
+    Parameters
+    ----------
+    text_instruction : :class:`monosashi.sequencer`
+        The text instruction to draw.
+
+    """
+
     def __init__(self, text_instruction: Text3d, **kwargs) -> None:
         super().__init__()
         self.text_instruction = text_instruction
 
     def draw(self, *args, **kwargs):
+        """Adds a text element with the text included in the provided text instruction.
+
+        Returns
+        -------
+        int
+            cadwork element ID of the added element.
+
+        """
         loc = self.text_instruction.location
         element_id = create_text_object(
             self.text_instruction.text,
