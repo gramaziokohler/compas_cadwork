5a19dbd4aa38642ca28ad668798356f7ae86a92a
Chen Kasirer <ckasirer@ethz.ch>
2023-06-22 17:37:57 +0200

    added some utility functions

--- src/compas_cadwork/utilities/__init__.py
+++ src/compas_cadwork/utilities/__init__.py
@@ -1,5 +1,7 @@
 import sys
 
+import utility_controller as uc
+
 
 def unload_module(module_name):
     """Unloads all loaded moduels which start with the given `module_name`"""
@@ -8,6 +10,18 @@ def unload_module(module_name):
         sys.modules.pop(module)
 
 
+def get_plugin_home() -> str:
+    """Returns the home root directory of the currently running plugin"""
+    return uc.get_plugin_path()
+
+
+def get_filename() -> str:
+    """Returns the name of the currently open cadwork document."""
+    return uc.get_3d_file_name()
+
+
 __all__ = [
     "unload_module",
+    "get_plugin_home",
+    "get_filename",
 ]
