9ce7c7baf998a4b4217ba8a4f58a98bd90c64f73
Chen Kasirer <ckasirer@ethz.ch>
2023-12-19 17:20:59 +0100

    added group type enum

--- src/compas_cadwork/datamodel/__init__.py
+++ src/compas_cadwork/datamodel/__init__.py
@@ -1,9 +1,11 @@
 from .element import Element
 from .element import ElementType
 from .element import ElementGroup
+from .element import ElementGroupingType
 
 __all__ = [
     "Element",
     "ElementType",
     "ElementGroup",
+    "ElementGroupingType",
 ]
--- src/compas_cadwork/datamodel/element.py
+++ src/compas_cadwork/datamodel/element.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from enum import auto
 from enum import Enum
+from enum import IntEnum
 from typing import Generator
 
 from compas.geometry import Frame
@@ -32,6 +33,13 @@ from bim_controller import get_ifc_base64_guid
 class StrEnum(str, Enum):
     """Why do *I* have to do this?"""
     pass
+
+
+class ElementGroupingType(IntEnum):
+    """CADWork Element Grouping Type"""
+    GROUP = 1
+    SUBGROUP = 2
+    NONE = 3
 
 
 class ElementType(StrEnum):
