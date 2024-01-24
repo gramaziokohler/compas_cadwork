2e9f14b313b3e382be1fa2c85c5b9b43d64f03ec
Chen Kasirer <ckasirer@ethz.ch>
2024-01-24 12:12:09 +0100

    duplicate key in setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
     author=about["__author__"],
     author_email=about["__author_email__"],
     url=about["__url__"],
-    license="MIT license",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
