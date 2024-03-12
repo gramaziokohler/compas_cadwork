29c9fddf33ec9ee980b270438186fdc3a5283f8d
Chen Kasirer <ckasirer@ethz.ch>
2024-03-12 20:24:18 +0100

    added new attributes and methods to Element

--- src/compas_cadwork/datamodel/__init__.py
+++ src/compas_cadwork/datamodel/__init__.py
@@ -2,10 +2,13 @@ from .element import Element
 from .element import ElementType
 from .element import ElementGroup
 from .element import ElementGroupingType
+from .element import ATTR_INSTRUCTION_ID
+
 
 __all__ = [
     "Element",
     "ElementType",
     "ElementGroup",
     "ElementGroupingType",
+    "ATTR_INSTRUCTION_ID",
 ]
