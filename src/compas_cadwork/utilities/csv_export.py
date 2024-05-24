0eb7d93cdb8aadd085c0417f35a9f7f65bec67cf
mhelmrei <helmreich@arch.ethz.ch>
2024-05-24 16:43:47 +0200

    added other list features plates and fasteners

--- src/compas_cadwork/utilities/csv_export.py
+++ src/compas_cadwork/utilities/csv_export.py
@@ -1,14 +1,26 @@
 import logging
 import os
+from enum import Enum
 from typing import List
 
+from list_controller import export_cover_list
 from list_controller import export_part_list
+from list_controller import export_production_list
 
 LOG = logging.getLogger(__name__)
 
 
+class ExportOption(Enum):
+    PART_LIST = "part_list"
+    COVER_LIST = "cover_list"
+    FASTENER_LIST = "fastener_list"
+
+    def __str__(self):
+        return self.value
+
+
 class CSVExporter:
-    """Used to export elements to csv files.
+    """Used to export elements to CSV files.
 
     Sets the required global settings for the export and restores them after the export.
 
@@ -17,23 +29,35 @@ class CSVExporter:
     def __init__(self):
         pass
 
-    def export_elements_to_CSV(self, element_ids: List[int], filepath: str):
+    def export_elements_to_CSV(self, element_ids: List[int], list_type: ExportOption, filepath: str):
         """Exports elements to CSV file.
 
         Parameters
         ----------
         element_ids : list(int)
             List of element ids to export.
+        list_type : str
+            Type of list to export (e.g., "part_list", "cover_list", "fastener_list").
         filepath : str
             Path to the resulting CSV file.
 
         """
         filepath = os.path.abspath(filepath)
         try:
-            LOG.debug(
-                f"""export_CSV_silently(element_i_ds={type(element_ids)}({type(element_ids[0])}), file_path={type(filepath)})"""
-            )
-            success = export_part_list(element_ids, filepath)
-            LOG.debug(f"export_CSV_silently: {success}")
+            LOG.debug(f"Exporting CSV: element_ids={element_ids}, list_type={list_type}, filepath={filepath}")
+            export_function = self.get_export_function(list_type)
+            success = export_function(element_ids, filepath)
+            LOG.debug(f"Export successful: {success}")
+        except FileNotFoundError as ex:
+            LOG.exception(f"File not found: {ex}")
         except Exception as ex:
-            LOG.exception(f"Failed to export elements to CSV. {str(ex)}")
+            LOG.exception(f"Failed to export elements to CSV: {ex}")
+
+    def get_export_function(self, list_type: str):
+        """Returns the export function based on the list type."""
+        export_functions = {
+            ExportOption.PART_LIST: export_part_list,
+            ExportOption.COVER_LIST: export_cover_list,
+            ExportOption.FASTENER_LIST: export_production_list,
+        }
+        return export_functions.get(list_type, None)
