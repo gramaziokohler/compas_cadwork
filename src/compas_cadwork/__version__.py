9742affec57574ad7e3ea0dc086a8e5ff8023637
Chen Kasirer <ckasirer@ethz.ch>
2024-08-08 09:28:34 +0200

    Bump version to 0.4.3

--- src/compas_cadwork/__version__.py
+++ src/compas_cadwork/__version__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 __title__ = "compas_cadwork"
 __description__ = "COMPAS package for integrating with cadwork"
 __url__ = "https://github.com/gramaziokohler/compas_cadwork"
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 __author__ = "Gramazio Kohler Research"
 __author_email__ = "gramaziokohler@arch.ethz.ch"
 __license__ = "MIT license"
