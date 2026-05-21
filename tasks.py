b514e1a325eedc05ff5c7048498de3446748ee6c
José Miguel Moreno <josemmo@pm.me>
2026-05-21 15:19:11 +0200

    Added custom prepare changelog task
    > Use custom implementation to avoid a Markdown linting issue

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from compas_invocations2.console import chdir
 from compas_invocations2.docs import docs
 from compas_invocations2.tests import testdocs
 from invoke.collection import Collection
@@ -7,10 +8,24 @@ from invoke.context import Context
 from invoke.tasks import task
 
 
+_UNRELEASED_CHANGELOG_TEMPLATE = "## Unreleased\n\n### Added\n\n### Changed\n\n### Removed\n\n## "
+
+
 @task
 def lint(ctx: Context) -> None:
     """Run all pre-commit hooks against the whole project."""
     ctx.run("pre-commit run --all-files")
+
+
+@task
+def prepare_changelog(ctx: Context) -> None:
+    """Prepare changelog for next release."""
+    with chdir(ctx.base_folder):
+        with open("CHANGELOG.md", "r+", newline="") as changelog:
+            content = changelog.read()
+            changelog.seek(0)
+            changelog.write(content.replace("## ", _UNRELEASED_CHANGELOG_TEMPLATE, 1))
+        ctx.run('git add CHANGELOG.md && git commit -m "Prepare changelog for next release"')
 
 
 @task
@@ -28,6 +43,7 @@ def test(ctx: Context) -> None:
 ns = Collection(
     docs,
     lint,
+    prepare_changelog,
     setup,
     test,
     testdocs,
