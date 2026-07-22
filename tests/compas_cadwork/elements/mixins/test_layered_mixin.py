e0e766c1d6a62dbd061084c876e9818fd23d2a15
José Miguel Moreno <jomoreno@ethz.ch>
2026-07-22 11:33:21 +0200

    Simplify LayeredMixin unit tests

--- tests/compas_cadwork/elements/mixins/test_layered_mixin.py
+++ tests/compas_cadwork/elements/mixins/test_layered_mixin.py
@@ -2,17 +2,13 @@ import pytest
 
 from compas_cadwork.elements.element import Element
 from compas_cadwork.elements.mixins.layered_mixin import LayeredMixin
-from compas_cadwork.materials.layer_stack import LayerStack
+from compas_cadwork.materials.layer_stack import WallLayerStack
 
 
-class DummyLayerStack(LayerStack):
-    pass
-
-
-class DummyElement(Element, LayeredMixin[DummyLayerStack]):
+class DummyElement(Element, LayeredMixin[WallLayerStack]):
     @property
-    def _layer_stack_type(self) -> type[DummyLayerStack]:
-        return DummyLayerStack
+    def _layer_stack_type(self) -> type[WallLayerStack]:
+        return WallLayerStack
 
 
 def test_gets_layers(cadwork) -> None:
@@ -26,8 +22,8 @@ def test_gets_layers(cadwork) -> None:
     element = DummyElement(123)
     cadwork.mlc.get_element_multi_layer_set.return_value = 1000
     layers = element.layers
-    assert layers == DummyLayerStack(1000)
-    assert type(layers) is DummyLayerStack
+    assert layers == WallLayerStack(1000)
+    assert type(layers) is WallLayerStack
 
 
 def test_sets_layers(cadwork) -> None:
@@ -39,5 +35,5 @@ def test_sets_layers(cadwork) -> None:
         element.layers = None
 
     # With value
-    element.layers = DummyLayerStack(1000)
+    element.layers = WallLayerStack(1000)
     cadwork.mlc.set_element_multi_layer_set.assert_called_once_with(123, 1000)
