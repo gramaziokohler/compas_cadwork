bce526375ecd72cc9634bcc6ce59a87dbfa2b738
José Miguel Moreno <jomoreno@ethz.ch>
2026-07-24 14:12:38 +0200

    Homogenize enum classes

--- src/compas_cadwork/materials/layer_type.py
+++ src/compas_cadwork/materials/layer_type.py
@@ -14,16 +14,6 @@ class LayerType(Enum):
     AIR = cadwork.multi_layer_type.air
     COVERING = cadwork.multi_layer_type.covering
 
-    def to_cadwork(self) -> cadwork.multi_layer_type:
-        """Convert value to Cadwork multi layer type.
-
-        Returns
-        -------
-        cadwork.multi_layer_type
-            Cadwork multi layer type.
-        """
-        return self.value
-
     @classmethod
     def from_cadwork(cls, raw_type: cadwork.multi_layer_type) -> Self:
         """Get value from Cadwork multi layer type.
@@ -44,6 +34,16 @@ class LayerType(Enum):
             If cannot determine the correct mapping type.
         """
         return cls(raw_type)
+
+    def to_cadwork(self) -> cadwork.multi_layer_type:
+        """Convert value to Cadwork multi layer type.
+
+        Returns
+        -------
+        cadwork.multi_layer_type
+            Cadwork multi layer type.
+        """
+        return self.value
 
     def __repr__(self) -> str:
         return f"<LayerType.{self.name}>"
